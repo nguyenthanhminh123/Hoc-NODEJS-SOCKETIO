e5d0801ad719299a14ce16b60673bc8c1df8eab8
ThanhMinh <thanhminh339968az@gmail.com>
2022-09-09 17:19:52 +0700

    add

---
+++ Bai-2/index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+app.use(express.static("./public")); 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+io.on("connection", function(socket){
+
+    console.log(" Béo đã kết nối : " + socket.id);
+
+    socket.on("CLIENT-SEND-MAU", function(data){
+        console.log(data);
+        io.socket.emit('SERVER-SEND-MAU', data)
+    });
+});
+
+app.get("/", function(req, res) {
+    res.render("trangchu");
+});
